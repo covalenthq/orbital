9cac96eee7322588b9eae041995d80ba130508b1
Levi Aul <levi@leviaul.com>
2021-01-13 18:31:43 -0800

    Add a (nonstandard) JSON6902Patch gsub op

--- lib/orbital/kustomize.rb
+++ lib/orbital/kustomize.rb
@@ -481,6 +481,35 @@ class Orbital::Kustomize::Json6902Patch::Add < Orbital::Kustomize::Json6902Patch
   end
 end
 
+class Orbital::Kustomize::Json6902Patch::Gsub < Orbital::Kustomize::Json6902Patch
+  def self.create(patch_spec)
+    self.new(
+      paths: patch_spec['paths'],
+      pattern: Regexp.new(patch_spec['pattern'], Regexp::EXTENDED),
+      replacement: patch_spec['replacement']
+    )
+  end
+
+  def initialize(paths:, pattern:, replacement:)
+    @lenses = paths.map{ |path| parse_lens(path) }
+    @pattern = pattern
+    @replacement = replacement
+  end
+
+  def apply(rc)
+    @lenses.inject(rc) do |doc, lens|
+      lens.update_in(doc) do |orig_value|
+        new_value = orig_value.gsub(@pattern, @replacement)
+
+        if new_value != orig_value
+          [:set, new_value]
+        else
+          :keep
+        end
+      end
+    end
+  end
+end
 
 class Orbital::Kustomize::Json6902Patch::Replace < Orbital::Kustomize::Json6902Patch
   def self.create(patch_spec)
